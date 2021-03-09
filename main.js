d9cfc6dfbe7bda4782cceb472418060137f087f9
Someshvar Sharan <75926693+Someshvar1@users.noreply.github.com>
2021-03-09 14:56:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ noseX=0;
 noseY=0;
 
 function preload() {
-  mustache_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup() {
   canvas = createCanvas(300, 300);
-  canvas.position(620,300);
+  canvas.position(630,250);
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
@@ -25,14 +25,14 @@ function gotPoses(results)
   if(results.length > 0)
   {
     console.log(results);
-    noseX =results[0].pose.nose.x-40;
-    noseY =results[0].pose.nose.y;
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
   }
 }
 
 function draw() {
   image(video, 0, 0, 300, 300);
-  image(mustache_nose,noseX,noseY,80,35);
+  image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){    
